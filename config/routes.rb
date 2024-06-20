eb28497ff4d612324ac0a1439009b0e05fec8411
jorge ochoa <jorge.uchija2021@gmail.com>
2024-06-19 21:43:28 -0400

    capitulos del 14 al 18

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get "products/new", to: "products#new", as: :new_product
+  post "/products", to: "products#create"
+  get "products/:id/edit", to: "products#edit", as: :edit_product
   get "/products", to: "products#index"
   get "/products/:id", to: "products#show", as: :product
+  patch "/products/:id", to: "products#update"
 end
