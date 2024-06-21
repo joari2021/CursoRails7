c4c2c97352464735412529f056a16335243813eb
jorge ochoa <jorge.uchija2021@gmail.com>
2024-06-20 22:29:14 -0400

    cap 21 al 23

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module AppCurso
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+ 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -23,5 +23,11 @@ module AppCurso
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # 
+    # available languages
+    config.i18n.available_locales = [:en, :es]
+
+    #lenguage by default
+    config.i18n.default_locale = :es
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get "products/new", to: "products#new", as: :new_product
-  post "/products", to: "products#create"
-  get "products/:id/edit", to: "products#edit", as: :edit_product
-  get "/products", to: "products#index"
-  get "/products/:id", to: "products#show", as: :product
-  patch "/products/:id", to: "products#update"
-  delete "/products/:id", to: "products#destroy"
+  resources :categories, except: :show
+  resources :products, path: "/"
 end
